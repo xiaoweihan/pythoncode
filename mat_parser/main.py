411e01b5e8659d35955e38f520fe650d55cd1faf
xiaowei_han <xiaowei_han@yeah.net>
2018-01-17 17:52:00 +0800

    commite some changes.

---
+++ mat_parser/main.py
@@ -0,0 +1,121 @@
+#coding=utf8
+import os
+import multiprocessing
+import shutil
+import re
+import argparse
+import mat_parser
+
+#递归遍历某个目录
+def _recursive_dir(dir_name,suffix_name):
+    '''
+
+    :param dir_name:
+    :return:
+    '''
+    if os.path.exists(dir_name) and os.path.isdir(dir_name):
+        g = os.walk(dir_name)
+        for path, d, filelist in g:
+            for filename in filelist:
+                matfile_name =  os.path.join(path, filename)
+                if os.path.splitext(matfile_name)[1] == suffix_name:
+                    yield matfile_name
+
+#过滤文件
+def _filter_file(file_name):
+    if os.path.exists(file_name) and os.path.isfile(file_name):
+        result = re.match('.*_(\d{4}.\d{1,2}.\d{1,2}.\d{1,2}.\d{1,2}.\d{1,2})',file_name)
+        if result:
+            return result.group(1) + '.h5'
+    return None
+
+
+#文件进行归类
+def _sort_files(target_dir):
+    file_list = os.listdir(target_dir)
+    file_sort_type = {}
+    for file_name in file_list:
+        if os.path.isfile(os.path.join(target_dir,file_name)):
+            result = re.match('^(\d{4}.\d{1,2}.\d{1,2})_.*', file_name)
+            if result:
+                if result.group(1) in file_sort_type:
+                    file_sort_type[result.group(1)].append(os.path.join(target_dir,file_name))
+                else:
+                    file_sort_type[result.group(1)] = []
+                    file_sort_type[result.group(1)].append(os.path.join(target_dir, file_name))
+
+    for key in file_sort_type:
+        dir_path = os.path.join(target_dir,key)
+
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+
+        for file_name in file_sort_type[key]:
+            shutil.move(file_name,dir_path)
+
+
+#进程函数
+def process_proc(**kwargs):
+    for file_name in kwargs['args']:
+        new_file_name = _filter_file(file_name)
+        if new_file_name:
+            h5_file_name = os.path.join(kwargs['dir'],new_file_name)
+            matConvert = mat_parser.Mat_Converter(file_name,h5_file_name,kwargs['compress_level'])
+            matConvert.yield_data_to_target_file()
+
+def main():
+    #begin parse the command line
+    command_parser = argparse.ArgumentParser(description='show the command usage')
+    command_parser.add_argument('--src','-s',help='specific a mat file dir',required=True)
+    command_parser.add_argument('--target', '-t', help='specific a target file dir', required=True)
+    #command_parser.add_argument('--variable', '-v', help='select variable name', action='append')
+    command_parser.add_argument('--compress', '-c', help='set compress level', type=int,choices=range(0,10))
+    args = command_parser.parse_args()
+
+    mat_file_dir = args.src
+    target_file_dir = args.target
+    compress_level = args.compress
+
+    if not os.path.exists(mat_file_dir):
+        print mat_file_dir,'is not exist.'
+        return
+    if not os.path.isdir(mat_file_dir):
+        print mat_file_dir,'is not a dir.'
+        return
+
+    if not os.path.exists(target_file_dir):
+        os.makedirs(target_file_dir)
+
+    # 递归遍历源目录
+    mat_file_list = list(_recursive_dir(mat_file_dir,'.mat'))
+
+    # 执行进程的最多个数
+    max_process_num = min(multiprocessing.cpu_count(), len(mat_file_list))
+
+    process_arg_array = []
+
+    per_process_arg_num = (len(mat_file_list) + max_process_num - 1) / max_process_num
+    x = 0
+    for _ in xrange(max_process_num):
+        process_tuple = tuple(mat_file_list[x:x + per_process_arg_num])
+        x += per_process_arg_num
+        tempdict = {}
+        tempdict['args'] = process_tuple
+        tempdict['dir'] = target_file_dir
+        tempdict['compress_level'] = compress_level
+        process_arg_array.append(tempdict)
+
+    process_array = []
+    for i in xrange(max_process_num):
+        p = multiprocessing.Process(target=process_proc, kwargs=process_arg_array[i])
+        p.start()
+        process_array.append(p)
+    for element in process_array:
+        element.join()
+
+    _sort_files(target_file_dir)
+
+    print 'congratulations,It is a good job!'
+
+if '__main__' == __name__:
+    main()
--- mat_parser/mat_parser.py
+++ mat_parser/mat_parser.py
@@ -4,19 +4,23 @@
 load the mat file and convert it's data to txt file
 '''
 import os
-import os.path
-import shutil
-import re
-import numpy
-import argparse
+import json
 import scipy.io
 import hd5_wrapper
 
 
+class MatKeyInfo(object):
+
+
+    def __init__(self,new_var_name,old_var_name,factor):
+        self.new_var_name = new_var_name
+        self.old_var_name = old_var_name
+        self.custom_factor = factor
+
 class Mat_Converter(object):
 
 
-    def __init__(self,mat_file_path = None,target_file_path = None,compress_level = 6):
+    def __init__(self,mat_file_path=None,target_file_path=None,compress_level=6):
         '''
 
         :param mat_file_path:
@@ -26,7 +30,7 @@ class Mat_Converter(object):
         #mat文件路径
         self.__mat_file_path = mat_file_path
         #读取数据名称集合
-        self.__key_list = None
+        self.__key_list = []
         #生成文件的路径
         self.__target_file_path = target_file_path
         #compress level
@@ -63,7 +67,16 @@ class Mat_Converter(object):
         matdata = scipy.io.loadmat(self.__mat_file_path)
         #Load the data
         value = matdata['DATA'][0,0]
-        self.__key_list = [x for x in value.dtype.names]
+
+        # 加载变量名映射表
+        json_path = os.getcwd() + r'\convert_table.json'
+
+        with open(json_path, 'r') as json_reader:
+            convert_table = json.load(json_reader)
+            for x in value.dtype.names:
+                if x in convert_table:
+                    self.__key_list.append(MatKeyInfo(convert_table[x]['new_name'],x,convert_table[x]['factor']))
+        #self.__key_list = [x for x in value.dtype.names]
 
 
         #开始生成hdf5
@@ -75,10 +88,11 @@ class Mat_Converter(object):
         write_data = {}
         sub_data = None
         for key in self.__key_list:
-            value_array = value[key]
+            value_array = value[key.old_var_name] * key.custom_factor
+            print type(value_array)
             #print value_array,type(value_array),value_array.shape
-            sub_data = hd5_wrapper.Hdf5Data(value_array,self.__compress_level)
-            write_data[key] = sub_data
+            sub_data = hd5_wrapper.Hdf5Data(value_array, self.__compress_level)
+            write_data[key.new_var_name] = sub_data
         hf5.data = write_data
         hf5.write_file()
     def _display(self):
@@ -87,96 +101,4 @@ class Mat_Converter(object):
         print self.__compress_level
 
 
-#递归遍历某个目录
-def _recursive_dir(dir_name,suffix_name):
-    '''
 
-    :param dir_name:
-    :return:
-    '''
-    if os.path.exists(dir_name) and os.path.isdir(dir_name):
-        g = os.walk(dir_name)
-        for path, d, filelist in g:
-            for filename in filelist:
-                matfile_name =  os.path.join(path, filename)
-                if os.path.splitext(matfile_name)[1] == suffix_name:
-                    yield matfile_name
-
-#过滤文件
-def _filter_file(file_name):
-    if os.path.exists(file_name) and os.path.isfile(file_name):
-        result = re.match('.*_(\d{4}.\d{1,2}.\d{1,2}.\d{1,2}.\d{1,2}.\d{1,2})',file_name)
-        if result:
-            return result.group(1) + '.h5'
-    return None
-
-
-#文件进行归类
-def _sort_files(target_dir):
-    file_list = os.listdir(target_dir)
-    file_sort_type = {}
-    for file_name in file_list:
-        if os.path.isfile(os.path.join(target_dir,file_name)):
-            result = re.match('^(\d{4}.\d{1,2}.\d{1,2})_.*', file_name)
-            if result:
-                if result.group(1) in file_sort_type:
-                    file_sort_type[result.group(1)].append(os.path.join(target_dir,file_name))
-                else:
-                    file_sort_type[result.group(1)] = []
-                    file_sort_type[result.group(1)].append(os.path.join(target_dir, file_name))
-
-
-
-    for key in file_sort_type:
-        dir_path = os.path.join(target_dir,key)
-
-        if not os.path.isdir(dir_path):
-            os.makedirs(dir_path)
-
-
-        for file_name in file_sort_type[key]:
-            shutil.move(file_name,dir_path)
-
-
-
-
-def main():
-    #begin parse the command line
-    command_parser = argparse.ArgumentParser(description='show the command usage')
-    command_parser.add_argument('--src','-s',help='specific a mat file dir',required=True)
-    command_parser.add_argument('--target', '-t', help='specific a target file dir', required=True)
-    #command_parser.add_argument('--variable', '-v', help='select variable name', action='append')
-    command_parser.add_argument('--compress', '-c', help='set compress level', type=int,choices=range(0,10))
-    args = command_parser.parse_args()
-
-    mat_file_dir = args.src
-    target_file_dir = args.target
-    compress_level = args.compress
-
-    if not os.path.exists(mat_file_dir):
-        print mat_file_dir,'is not exist.'
-        return
-    if not os.path.isdir(mat_file_dir):
-        print mat_file_dir,'is not a dir.'
-        return
-
-    if not os.path.exists(target_file_dir):
-        os.makedirs(target_file_dir)
-
-    #create a parser
-    matConvert = Mat_Converter(compress_level=compress_level)
-    for element in _recursive_dir(mat_file_dir,'.mat'):
-        src_file_name = element
-        target_file_name = _filter_file(src_file_name)
-        if target_file_name:
-            matConvert.mat_file_path = src_file_name
-            matConvert.target_file_path = os.path.join(target_file_dir,target_file_name)
-            matConvert.yield_data_to_target_file()
-
-
-    _sort_files(target_file_dir)
-
-    print 'congratulations,It is a good job!'
-
-if '__main__' == __name__:
-    main()
